7753e3de9cb68200a9feef1f2a1e2e7446e90795
enriquema <e.mora.alonso@gmail.com>
2024-08-20 19:07:50 +0200

    removing secret from settings.py

--- management/settings.py
+++ management/settings.py
@@ -19,8 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)gtn!kr#ec7os4le++c%i+84sy!-dzgn3-h0dkxxkhq9oqs0$#'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
