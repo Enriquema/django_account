275ae86e8bdd54ae57c945c5443f897376582e36
enriquema <e.mora.alonso@gmail.com>
2024-08-21 13:06:46 +0200

    removing unused things and modifying register and login if user was authenticated

--- account/views.py
+++ account/views.py
@@ -1,29 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
-
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                login(request, user)
-                return HttpResponse('Authenticated successfully')
-            else:
-                return HttpResponse('Disabled account')
-        else:
-            return HttpResponse('Invalid login')
-    else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
 
 @login_required
 def dashboard(request):
